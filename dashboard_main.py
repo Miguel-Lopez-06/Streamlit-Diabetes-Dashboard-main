234f3eeaa774d1789638739e7f63e0a9c069489a
Miguel-Lopez-06 <vxleugim@gmail.com>
2024-11-10 20:50:44 +0800

    update

--- dashboard_main.py
+++ dashboard_main.py
@@ -84,7 +84,7 @@ with st.sidebar:
     st.markdown("📊 [Dataset](https://www.kaggle.com/datasets/arshid/iris-flower-dataset)")
     st.markdown("📗 [Google Colab Notebook](https://colab.research.google.com/drive/1KJDBrx3akSPUW42Kbeepj64ZisHFD-NV?usp=sharing)")
     st.markdown("🐙 [GitHub Repository](https://github.com/Zeraphim/Streamlit-Iris-Classification-Dashboard)")
-    st.markdown("by: [`Zeraphim`](https://jcdiamante.com)")
+    st.markdown("by: [`AM7-Group 6`](https://jcdiamante.com)")
 
 # -------------------------
 
