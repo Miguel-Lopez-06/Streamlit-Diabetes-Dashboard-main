9e740116232f4e87864fb68393c095473bc63d25
Miguel-Lopez-06 <vxleugim@gmail.com>
2024-11-13 10:33:39 +0800

    update

--- dashboard_main.py
+++ dashboard_main.py
@@ -205,7 +205,7 @@ elif st.session_state.page_selection == "eda":
 
     col = st.columns((5, 5, 5), gap='medium')
 
-    with col[0]:
+    with col[5]:
 
         with st.expander('Legend', expanded=True):
             st.write('''
