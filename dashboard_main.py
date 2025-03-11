8e98f6ef467963373ce6e8e64ec83b2fe4ce2c49
Miguel-Lopez-06 <vxleugim@gmail.com>
2025-03-12 06:34:48 +0800

    update

--- dashboard_main.py
+++ dashboard_main.py
@@ -629,7 +629,7 @@ elif  st.session_state.page_selection == "prediction":
 
          # Input boxes for the features
         log_Pregnancies = st.number_input('Pregnancies', min_value=0, max_value=100, step=1, key='log_Pregnancies', value=0 if st.session_state.clear else st.session_state.get('log_Pregnancies', 0.0))
-        log_Glucose = st.number_input('Glucose', min_value=0.0, max_value=10.0, step=0.1, key='log_Glucose', value=0.0 if st.session_state.clear else st.session_state.get('log_Glucose', 0.0))
+        log_Glucose = st.number_input('Glucose', min_value=0, max_value=100, step=1, key='log_Glucose', value=0 if st.session_state.clear else st.session_state.get('log_Glucose', 0.0))
         log_BloodPressure = st.number_input('BloodPressure', min_value=0.0, max_value=10.0, step=0.1, key='log_BloodPressure', value=0.0 if st.session_state.clear else st.session_state.get('log_BloodPressure', 0.0))
         log_SkinThickness = st.number_input('SkinThickness', min_value=0.0, max_value=10.0, step=0.1, key='log_SkinThickness', value=0.0 if st.session_state.clear else st.session_state.get('log_SkinThickness', 0.0))
         log_Insulin = st.number_input('Insulin', min_value=0.0, max_value=10.0, step=0.1, key='log_Insulin', value=0.0 if st.session_state.clear else st.session_state.get('log_Insulin', 0.0))
